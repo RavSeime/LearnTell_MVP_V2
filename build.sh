dfabc1619c09f8257010a2c59f6305caa18b286e
RavSeime <ravseime@gmail.com>
2025-12-02 13:04:07 +0100

    Fixed cd issue in build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
+cd django_stack
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-#python manage.py migrate
+python manage.py migrate 
