542f289a24cbb1a86b40a83143b43d39e4408de8
RavSeime <ravseime@gmail.com>
2026-02-03 07:56:21 +0100

    V9

--- django_stack/survey_taker/views.py
+++ django_stack/survey_taker/views.py
@@ -66,6 +66,7 @@ GRAPH_MODULES = {
     "gatekeeper_engagement_validation_v6": "survey_taker.graphs.gatekeeper_engagement_validation_v6",
     "gatekeeper_engagement_validation_v7": "survey_taker.graphs.gatekeeper_engagement_validation_v7",
     "gatekeeper_engagement_validation_v8": "survey_taker.graphs.gatekeeper_engagement_validation_v8",
+    "gatekeeper_engagement_validation_v9": "survey_taker.graphs.gatekeeper_engagement_validation_v9",
     "gatekeeper_engagement_validation": "survey_taker.graphs.gatekeeper_engagement_validation",
 }
 
